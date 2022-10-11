ec9658360364716c79e718449468f91c3ed7116a
DanielQuilce <danielquilcue18@gmail.com>
2022-10-11 18:12:38 -0500

    Finished project

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    const message = document.getElementById('result');
+    message.innerHTML = "¡¡Apapacho enviado!!"
+}, 6200);
+
+setTimeout(function() {
+    const message = document.getElementById('state-message');
+    message.innerHTML = "&nbsp;"
+}, 6000);
